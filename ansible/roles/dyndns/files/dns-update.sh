83d7fd2bd5c840df13c1983ea7e01075b589387f
Tim McCormack <cortex@brainonfire.net>
2018-03-14 15:16:31 -0400

    Better dyndns logging (timestamp; JSON output); timeout on requests

--- ansible/roles/dyndns/files/dns-update.sh
+++ ansible/roles/dyndns/files/dns-update.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Randomized [0..59] sleep to reduce impact on afraid.org
 sleep 20
 
+logfile=/var/log/dyndns-afraid.org-home.log
+
 token="$(cat /opt/commdata/dyndns/secrets/afraid.org-token-home)"
-curl -sS "https://sync.afraid.org/u/$token/" >> /var/log/dyndns-afraid.org-home.log 2>&1
+
+echo -n "`date -u +'%Y-%m-%d %H:%M:%S'`: " >> $logfile
+curl -sS "https://sync.afraid.org/u/$token/?content-type=json" -m10 \
+     >> $logfile 2>&1
