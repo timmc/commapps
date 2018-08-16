139bb09deea4a5b321ea70565b8a6c63076c8eee
Tim McCormack <cortex@brainonfire.net>
2018-08-15 22:36:09 -0400

    clean up archive listing script; fix logging bug (no log function here)

--- ansible/roles/sandstorm/files/tarsnap/list-archives.sh
+++ ansible/roles/sandstorm/files/tarsnap/list-archives.sh
@@ -2,11 +2,8 @@
 # List backups of community data
 
 mountpoint -q /srv/commdata || {
-  log "ERROR: Community data is not yet mounted"
+  echo "ERROR: Community data is not yet mounted"
   exit 2
 }
 
-tarsnap --list-archives \
-        --keyfile /srv/commdata/backups/tarsnap-rw.key \
-        --humanize-numbers \
-    | sort
+tarsnap --list-archives --keyfile /srv/commdata/backups/tarsnap-rw.key | sort
