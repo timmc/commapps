91166ded849f8d214a3d0c2d92c97468aef952ec
Tim McCormack <cortex@brainonfire.net>
2018-08-16 10:25:03 -0400

    Script for restoring archives

---
+++ ansible/roles/sandstorm/files/tarsnap/restore-archive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Restore from an archive of community data
+#
+# Usage: $0 <archive-name>
+
+if [[ "$#" != "1" ]]; then
+    echo "ERROR: Must pass archive-name argument"
+    exit 1
+fi
+
+ARCHIVE="$1"
+
+# Check if commdata is mounted before proceeding
+
+mountpoint -q /srv/commdata || {
+  echo "ERROR: Community data is not yet mounted"
+  exit 2
+}
+
+TARGET="/srv/commdata/tmp/tarsnap-restore/$ARCHIVE"
+
+mkdir -p "$TARGET"
+tarsnap -x -f "$ARCHIVE" -C "$TARGET" \
+        --keyfile /srv/commdata/backups/tarsnap-rw.key \
+        -p -v
