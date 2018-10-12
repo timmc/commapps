7fb3bd06b101915adf5bc7337f4ccd96f80eaa6a
Tim McCormack <cortex@brainonfire.net>
2018-10-11 22:12:57 -0400

    Installer for Sandstorm

---
+++ ansible/roles/sandstorm/files/sandstorm/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+install_to="$1"
+
+installer_file="$(mktemp sandstorm-installer.sh.XXXXXXXX)"
+wget --output-document="$installer_file" --timeout=60 \
+     "https://raw.githubusercontent.com/sandstorm-io/sandstorm/master/install.sh"
+
+# The -d flag says to accept all defaults (non-interactive mode)
+export OVERRIDE_SANDSTORM_DEFAULT_DIR="$install_to"
+export REPORT=no
+bash -- "$installer_file" -d
