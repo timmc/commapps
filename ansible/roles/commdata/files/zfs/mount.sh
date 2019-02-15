0c18660577e485311df5ac18969ccd530a2d064c
Tim McCormack <cortex@brainonfire.net>
2019-02-15 07:48:49 -0500

    Properly handle case where zfs import was tried before LUKS was up
    This didn't seem to happen every time; perhaps there was a race condition.
    But last night the server was rebooted and didn't come up properly; the
    zpool's health status was "FAULTED" and claimed it had been unable to
    read from the (missing) LUKS device.

--- ansible/roles/commdata/files/zfs/mount.sh
+++ ansible/roles/commdata/files/zfs/mount.sh
@@ -8,7 +8,16 @@ set -u
 # been imported, for instance on initial setup.
 
 pool_status=`zpool list -H -o health commdata 2>/dev/null || true`
-if [[ "$pool_status" != "ONLINE" ]]; then
-    # Automatically mounts
-    zpool import commdata
+
+if [[ "$pool_status" == "ONLINE" ]]; then
+    exit 0
 fi
+
+if [[ "$pool_status" == "FAULTED" ]]; then
+    # Maybe it tried to mount before LUKS was open and failed. Let's
+    # try to reset it so it can mount.
+    zpool export commdata || true
+fi
+
+# Automatically mounts
+zpool import commdata
