84a975dd894a7bdcf850248b7a460427c8dc5280
Tim McCormack <cortex@brainonfire.net>
2020-09-10 21:44:04 -0400

    Grain site to support integration testing of Spelunk's WebDAV support

--- ansible/roles/sandstorm/files/certbot/scripts/renew-certs.sh
+++ ansible/roles/sandstorm/files/certbot/scripts/renew-certs.sh
@@ -9,3 +9,7 @@ RENEW_SUB=sandy RENEW_BASE=parsni.ps RENEW_WILD=true \
 
 RENEW_SUB=sandstorm RENEW_BASE=appux.com RENEW_WILD=true \
  /opt/commapps/certbot/scripts/renew-one-cert.sh
+
+# For integration testing of Spelunk's WebDAV support
+RENEW_SUB=spelunk-webdav-testing RENEW_BASE=spidersge.org \
+ /opt/commapps/certbot/scripts/renew-one-cert.sh
