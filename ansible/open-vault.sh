77a5e37ea936e894834b1eb8574dc71b930e35ac
Tim McCormack <cortex@brainonfire.net>
2020-12-21 19:39:06 -0500

    Quieter open-vault.sh, and some cleanup
    - Separate line for defaulting
    - Combine echoes
    - Print messages to stderr

--- ansible/open-vault.sh
+++ ansible/open-vault.sh
@@ -8,8 +8,8 @@ set -eu -o pipefail
 # https://github.com/yaegashi/ansible-snippets/blob/master/gnupg/ansible-gpg-file.sh
 
 if [ -z "$GPG_TTY" ]; then
-    echo "Error: GPG_TTY variable must be set for gpg-agent to work"
-    cat << EOF
+    cat << EOF >&2
+Error: GPG_TTY variable must be set for gpg-agent to work
 # The following must be in .bashrc or similar:
 GPG_TTY=\$(tty)
 export GPG_TTY
@@ -17,5 +17,5 @@ EOF
     exit 1
 fi
 
-gpg --batch --use-agent --decrypt -- \
-    "${VAULT_PASSPHRASE_GPG_FILE:-$HOME/secrets/appux/vault-passphrase.gpg}"
+pass_file="${VAULT_PASSPHRASE_GPG_FILE:-$HOME/secrets/appux/vault-passphrase.gpg}"
+gpg --batch --use-agent --decrypt --quiet -- "$pass_file"
