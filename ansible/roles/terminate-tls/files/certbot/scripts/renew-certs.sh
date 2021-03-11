46c4b2a8d260f9f5ced52ce5b8d0eef12acf7f0d
Tim McCormack <cortex@brainonfire.net>
2021-03-10 20:39:35 -0500

    Separate cert renewal config from scripting
    - terminate-tls role makes a dir for renewal config JSON files for
      renew-certs.sh to read
    - sandstorm role drops files into that dir and re-runs script if files
      are added or change

--- ansible/roles/terminate-tls/files/certbot/scripts/renew-certs.sh
+++ ansible/roles/terminate-tls/files/certbot/scripts/renew-certs.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-# Renew certs required for Sandstorm, including wildcard cert, using
-# NearlyFreeSpeech.net API.
+# Renew certs listed in domains directory.
 
 set -eu -o pipefail
 
-RENEW_SUB=sandstorm RENEW_BASE=appux.com RENEW_WILD=true \
- /opt/commapps/certbot/scripts/renew-one-cert.sh
+function renew_for_file {
+    filepath="$1"
+    export RENEW_SUB="$(jq -r .sub < "$filepath")"
+    export RENEW_BASE="$(jq -r .base < "$filepath")"
+    export RENEW_WILD="$(jq -r .wild < "$filepath")"
+    echo >&2 "Running renewal check with configuration for $RENEW_SUB.$RENEW_BASE (wild=$RENEW_WILD)"
+    /opt/commapps/certbot/scripts/renew-one-cert.sh
+}
 
-# For integration testing of Spelunk's WebDAV support
-RENEW_SUB=spelunk-webdav-testing RENEW_BASE=spidersge.org \
- /opt/commapps/certbot/scripts/renew-one-cert.sh
+failed=
+find /opt/commapps/certbot/domains.d -maxdepth 1 -name '*.json' -type f | while IFS= read -r f; do
+    echo >&2 "Found configuration file $f"
+    renew_for_file "$f" || {
+        failed=true # delay exit code
+        echo >&2 "Renewal failed! Moving on to next configuration."
+    }
+done
+
+if [[ "$failed" = "true" ]]; then
+    exit 1
+fi
